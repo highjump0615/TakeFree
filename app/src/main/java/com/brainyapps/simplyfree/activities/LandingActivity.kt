5297183bbd6e9c3ce754a9070aa7bd7a73bdc0f1
highjump <lixiang901112@163.com>
2018-03-25 01:31:25 +0800

    Added UI for landing page

--- app/src/main/java/com/brainyapps/simplyfree/activities/LandingActivity.kt
+++ app/src/main/java/com/brainyapps/simplyfree/activities/LandingActivity.kt
@@ -2,12 +2,23 @@ package com.brainyapps.simplyfree.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.brainyapps.simplyfree.R
+import kotlinx.android.synthetic.main.activity_landing.*
 
-class LandingActivity : AppCompatActivity() {
+class LandingActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+        this.but_login.setOnClickListener(this)
+        this.but_signup.setOnClickListener(this)
+        this.but_forget.setOnClickListener(this)
+        this.layout_but_fb.setOnClickListener(this)
+        this.layout_but_gplus.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
     }
 }
