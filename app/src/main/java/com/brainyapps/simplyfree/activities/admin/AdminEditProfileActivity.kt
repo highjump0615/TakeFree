e9bd45cadcdf21e88016dbbc594814549ae89982
highjump <lixiang901112@163.com>
2018-05-01 11:41:15 +0800

    Added admin setting & edit profile module

--- app/src/main/java/com/brainyapps/simplyfree/activities/admin/AdminEditProfileActivity.kt
+++ app/src/main/java/com/brainyapps/simplyfree/activities/admin/AdminEditProfileActivity.kt
@@ -2,13 +2,17 @@ package com.brainyapps.simplyfree.activities.admin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import com.brainyapps.simplyfree.R
 import com.brainyapps.simplyfree.activities.BaseActivity
+import com.brainyapps.simplyfree.activities.BaseEditProfileActivity
+import com.brainyapps.simplyfree.activities.LoginBaseActivity
+import com.brainyapps.simplyfree.models.User
 import com.brainyapps.simplyfree.utils.Utils
 import kotlinx.android.synthetic.main.activity_admin_edit_profile.*
 
-class AdminEditProfileActivity : BaseActivity(), View.OnClickListener  {
+class AdminEditProfileActivity : BaseEditProfileActivity(), View.OnClickListener  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +20,34 @@ class AdminEditProfileActivity : BaseActivity(), View.OnClickListener  {
 
         setNavbar("Edit Profile", true)
 
+        initUserInfo()
+
         this.but_save.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.but_save -> {
-                finish()
+                if (!checkPasswordValidate()) {
+                    return
+                }
+
+                if (TextUtils.isEmpty(editPassword!!.text.toString())) {
+                    finish()
+                    return
+                }
+
+                Utils.createProgressDialog(this, "Updating Profile", "Saving your user information")
+
+                savePassword(object: PasswordSaveListener {
+                    override fun onSavedPassword(success: Boolean) {
+                        Utils.closeProgressDialog()
+
+                        if (success) {
+                            finish()
+                        }
+                    }
+                })
             }
         }
     }
--- app/src/main/java/com/brainyapps/simplyfree/activities/admin/AdminSettingsActivity.kt
+++ app/src/main/java/com/brainyapps/simplyfree/activities/admin/AdminSettingsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import com.brainyapps.simplyfree.R
 import com.brainyapps.simplyfree.activities.BaseActivity
+import com.brainyapps.simplyfree.activities.LandingActivity
 import com.brainyapps.simplyfree.utils.Utils
 import kotlinx.android.synthetic.main.activity_admin_settings.*
 
@@ -17,12 +18,19 @@ class AdminSettingsActivity : BaseActivity(), View.OnClickListener {
         setNavbar("Settings", true)
 
         this.text_edit_profile.setOnClickListener(this)
+        this.but_logout.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.text_edit_profile -> {
                 Utils.moveNextActivity(this, AdminEditProfileActivity::class.java)
+            }
+
+            // log out
+            R.id.but_logout -> {
+                signOutClear()
+                Utils.moveNextActivity(this, LandingActivity::class.java, true, true)
             }
         }
     }
