259455ae71aa7f06aadc738da25a097206ce0c64
highjump <lixiang901112@163.com>
2018-06-01 22:30:43 +0800

    Added moudle for admin user detail

--- app/src/main/java/com/brainyapps/simplyfree/activities/admin/AdminUserDetailActivity.kt
+++ app/src/main/java/com/brainyapps/simplyfree/activities/admin/AdminUserDetailActivity.kt
@@ -1,15 +1,47 @@
 package com.brainyapps.simplyfree.activities.admin
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import com.brainyapps.simplyfree.R
 import com.brainyapps.simplyfree.activities.BaseActivity
+import com.brainyapps.simplyfree.helpers.UserDetailHelper
+import com.brainyapps.simplyfree.models.User
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.activity_admin_user_detail.*
 
 class AdminUserDetailActivity : BaseActivity() {
+
+    lateinit var user: User
+    lateinit var banHelper: AdminBanUserHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_user_detail)
 
-        setNavbar("Bill Watson", true)
+        // get user from intent
+        val bundle = intent.extras
+        user = bundle?.getParcelable<User>(UserDetailHelper.KEY_USER)!!
+
+        setNavbar(user.userFullName(), true)
+
+        //
+        // fill user info
+        //
+
+        // photo
+        Glide.with(this)
+                .load(user.photoUrl)
+                .apply(RequestOptions.placeholderOf(R.drawable.user_default).fitCenter())
+                .into(imgview_user)
+
+        // name
+        edit_name.text = user.userFullName()
+
+        // email
+        edit_email.text = user.email
+
+        banHelper = AdminBanUserHelper(this, user)
     }
 }
